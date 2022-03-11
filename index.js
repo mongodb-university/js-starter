b761ff66112e8da05d80c9135f0be54495e24366
Shivamm117 <shivam.mahajan117@gmail.com>
2022-03-11 06:17:45 +0530

    gg

---
+++ index.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://Shivam:Shivam@cluster0.zlgvg.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
